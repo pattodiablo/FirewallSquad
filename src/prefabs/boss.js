b6948c3cd710cd43a28828754e992674c0d40f98
pattodiablo <pattodiablo@gmail.com>
2022-05-14 15:36:59 -0500

    prefabs

---
+++ src/prefabs/boss.js
@@ -0,0 +1,25 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class boss extends Phaser.GameObjects.Sprite {
+
+	constructor(scene, x, y, texture, frame) {
+		super(scene, x ?? 165, y ?? 140, texture || "boss", frame);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
---
+++ src/prefabs/enemy5.js
@@ -0,0 +1,25 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class enemy5 extends Phaser.GameObjects.Sprite {
+
+	constructor(scene, x, y, texture, frame) {
+		super(scene, x ?? 105, y ?? 113, texture || "enemy5", frame);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
