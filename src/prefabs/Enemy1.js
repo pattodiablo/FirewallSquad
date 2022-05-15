ccd20f738db45ad7a0d16681ba5f591bf0ce5b10
Lacaspa <diegojleon@hotmail.com>
2022-05-14 21:35:31 -0500

    enemy 1

--- src/prefabs/Enemy1.js
+++ src/prefabs/Enemy1.js
@@ -18,16 +18,54 @@ class Enemy1 extends Phaser.GameObjects.Sprite {
 
 	create(){
 		this.scene.physics.world.enableBody(this);
-		//console.log(Phaser.Math.Angle.Between(this.x,this.y,this.scene.player.x,this.scene.player.y))
+
+		this.scene.physics.add.overlap(this.scene.player, this,this.playerCollide);
+		this.scene.physics.add.overlap(this.scene.playerBullets, this,this.enemyDestroy);
+		this.setScale(0.1)
+		this.animarNacimiento()
+
 		this.vel = Phaser.Math.Between(50,200);
-		//this.body.velocity.x=10//Phaser.Math.Between(150,300)*(this.scene.player.x-this.x)/Math.sqrt((this.x-this.scene.player.x)*(this.x-this.scene.player.x)+(this.y-this.scene.player.y)*(this.y-this.scene.player.y))
+		
 	}
 
 	update(){
-	this.angle=90+(180/Math.PI)*Phaser.Math.Angle.Between(this.x,this.y,this.scene.player.x,this.scene.player.y); 
-	this.scene.physics.velocityFromAngle(-90+this.angle, this.vel , this.body.velocity);
+		if(this.active){
+			this.angle=90+(180/Math.PI)*Phaser.Math.Angle.Between(this.x,this.y,this.scene.player.x,this.scene.player.y); 
+			this.scene.physics.velocityFromAngle(-90+this.angle, this.vel , this.body.velocity);
+		}
+		
+	
+	
 	//console.log(this.angle);
     }
+
+	animarNacimiento(){
+
+		var entrandoTimeline = this.scene.tweens.createTimeline();
+		entrandoTimeline.add({
+			targets: this,
+			scale: 1,
+			duration: 100,
+			ease: 'Linear',
+			repeat: 0
+
+		});
+		entrandoTimeline.play();
+
+	}
+
+	enemyDestroy(bullet,enemy){
+		//poner sonido
+	
+		enemy.scene.player.handleScore(enemy);
+		enemy.destroy();
+	}
+
+	playerCollide(player,enemy){
+	enemy.destroy();
+		player.handleEnemyCollition();
+		
+	}
 
 	/* END-USER-CODE */
 }
