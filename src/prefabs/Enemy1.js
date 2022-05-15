2c77fd64faa62ce339d2fca4005cb718adf6fba9
Lacaspa <diegojleon@hotmail.com>
2022-05-14 20:50:27 -0500

    enemy1

--- src/prefabs/Enemy1.js
+++ src/prefabs/Enemy1.js
@@ -10,6 +10,7 @@ class Enemy1 extends Phaser.GameObjects.Sprite {
 
 		/* START-USER-CTR-CODE */
 		this.updateEvent = this.scene.events.once("update", () => this.create());
+		this.updateEvent = this.scene.events.on("update", () => this.update());
 		/* END-USER-CTR-CODE */
 	}
 
@@ -17,12 +18,16 @@ class Enemy1 extends Phaser.GameObjects.Sprite {
 
 	create(){
 		this.scene.physics.world.enableBody(this);
-		this.body.velocity.x+=Phaser.Math.Between(20,150);
-		
+		//console.log(Phaser.Math.Angle.Between(this.x,this.y,this.scene.player.x,this.scene.player.y))
+		this.vel = Phaser.Math.Between(50,200);
+		//this.body.velocity.x=10//Phaser.Math.Between(150,300)*(this.scene.player.x-this.x)/Math.sqrt((this.x-this.scene.player.x)*(this.x-this.scene.player.x)+(this.y-this.scene.player.y)*(this.y-this.scene.player.y))
 	}
 
 	update(){
-	}
+	this.angle=90+(180/Math.PI)*Phaser.Math.Angle.Between(this.x,this.y,this.scene.player.x,this.scene.player.y); 
+	this.scene.physics.velocityFromAngle(-90+this.angle, this.vel , this.body.velocity);
+	//console.log(this.angle);
+    }
 
 	/* END-USER-CODE */
 }
--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -4,67 +4,70 @@
 /* START OF COMPILED CODE */
 
 class Level extends Phaser.Scene {
-
+	
 	constructor() {
 		super("Level");
-
+		
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
-
-	/** @returns {void} */
+	
 	editorCreate() {
-
+		
 		// background
 		const background = this.add.tileSprite(0, 0, 64, 64, "background");
 		background.setOrigin(0, 0);
-
+		
 		// player
 		const player = new Player(this, 296, 276);
 		this.add.existing(player);
-
+		
 		// heart
 		const heart = new Heart(this, 26, 23);
 		this.add.existing(heart);
-
+		
 		// heart_1
 		const heart_1 = new Heart(this, 61, 22);
 		this.add.existing(heart_1);
-
+		
 		// heart_2
 		const heart_2 = new Heart(this, 94, 22);
 		this.add.existing(heart_2);
-
+		
 		// Score
 		const score = this.add.text(602, 15, "", {});
+		score.setOrigin(0.5, 0.5);
 		score.text = "SCORE";
-		score.setStyle({ "color": "#ff0048", "fontFamily": "KANIT", "fontSize": "20px", "stroke": "#" });
-
+		score.setStyle({"color":"#ff0048","fontFamily":"KANIT","fontSize":"20px","stroke":"#"});
+		
 		// Ultimate
 		const ultimate = this.add.text(342, 413, "", {});
+		ultimate.setOrigin(0.5, 0.5);
 		ultimate.text = "ULTIMATE DEFENSE";
-		ultimate.setStyle({ "color": "#ff0048", "fontFamily": "KANIT", "fontSize": "20px" });
-
+		ultimate.setStyle({"color":"#ff0048","fontFamily":"KANIT","fontSize":"20px"});
+		
 		// Counter
 		const counter = this.add.text(690, 18, "", {});
+		counter.setOrigin(0.5, 0.5);
 		counter.text = "00000";
-		counter.setStyle({ "color": "#0000ff", "fontFamily": "KANIT" });
-
+		counter.setStyle({"color":"#0000ff","fontFamily":"KANIT"});
+		
 		// rectangle_1
 		const rectangle_1 = this.add.rectangle(711, 24, 70, 30);
 		rectangle_1.fillColor = 16711752;
 		rectangle_1.isStroked = true;
 		rectangle_1.strokeColor = 16711752;
-
+		
 		this.background = background;
-
-		this.events.emit("scene-awake");
+		this.player = player;
 	}
-
+	
 	/** @type {Phaser.GameObjects.TileSprite} */
 	background;
-
+	/** @type {Player} */
+	player;
+	
 	/* START-USER-CODE */
 
 	// Write more your code here
@@ -84,13 +87,14 @@ class Level extends Phaser.Scene {
 			loop: true
 		});
 
-		this.createEnemy1Timer = this.time.addEvent({
-			delay: 2000,                // ms
+		this.createEnemy3Timer = this.time.addEvent({
+			delay: 1500,                // ms
 			callback: this.crearEnemy1,
 			//args: [],
 			callbackScope: this,
 			loop: true
 		});
+
 
 	}
 
@@ -100,14 +104,15 @@ class Level extends Phaser.Scene {
 
 	crearEnemy3(){
 
-		const enemy3 = new Enemy3(this, Phaser.Math.FloatBetween(0,document.body.clientWidth), Phaser.Math.FloatBetween(0,document.body.clientHeight));
-		this.add.existing(enemy3);
+		//const enemy3 = new Enemy3(this, Phaser.Math.FloatBetween(0,document.body.clientWidth), Phaser.Math.FloatBetween(0,document.body.clientHeight));
+		//this.add.existing(enemy3);
 	}
 	crearEnemy1(){
-		const enemy1 = new Enemy1(this,Phaser.Math.FloatBetween(0,document.body.clientWidth), Phaser.Math.FloatBetween(0,document.body.clientHeight))
-		this.add.existing(enemy1);
+		//const enemy1 = new Enemy1(this,Phaser.Math.FloatBetween(0,document.body.clientWidth), Phaser.Math.FloatBetween(0,document.body.clientHeight))
+		//this.add.existing(enemy1);
 
 	}
+
 
 	/* END-USER-CODE */
 }
